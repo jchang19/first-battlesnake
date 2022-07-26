a74fae02828c08c21d4316e18485b05b2a9fa546
Justin Tundra Chang <justin.tundra@gmail.com>
2022-07-26 16:42:40 -0400

    customization

--- src/logic.py
+++ src/logic.py
@@ -17,10 +17,10 @@ def get_info() -> dict:
     """
     return {
         "apiversion": "1",
-        "author": "",  # TODO: Your Battlesnake Username
-        "color": "#888888",  # TODO: Personalize
-        "head": "default",  # TODO: Personalize
-        "tail": "default",  # TODO: Personalize
+        "author": "tundra",  # TODO: Your Battlesnake Username
+        "color": "#d9b3ff",  # TODO: Personalize
+        "head": "gamer",  # TODO: Personalize
+        "tail": "mouse",  # TODO: Personalize
     }
 
 
